5ecc33927230d63b8d1cd158d72b96b70050169f
Gary Leong <gwleong@gmail.com>
2015-04-21 21:17:18 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'wpostgres'
+maintainer       'Gary Leong'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures postgres'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
+depends 'postgres'
+
